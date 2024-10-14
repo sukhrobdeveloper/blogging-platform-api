5fb1872150a223143d0100289065e6efa8165688
sukhrobdeveloper <sukhrobtokhirov006@gmail.com>
2024-10-15 04:20:11 +0500

    Initial commit

---
+++ src/test/java/misu/dev/blogging_platform_api/BloggingPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package misu.dev.blogging_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
